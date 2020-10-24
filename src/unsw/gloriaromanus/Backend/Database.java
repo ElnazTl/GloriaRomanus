fdcbe5de0f0d57e0aa0f2e4ee94ce360ca8460aa
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-10-25 08:42:46 +1100

    added extra fuctions to database

--- src/unsw/gloriaromanus/Backend/Database.java
+++ src/unsw/gloriaromanus/Backend/Database.java
@@ -10,11 +10,34 @@ import java.nio.file.Paths;
 import java.util.Optional;
 
 
+import java.util.Map;
+import java.util.HashMap;
 
 
 public class Database {
 
-    public void addFraction(String faction) throws IOException {
+    public Map<Province,Unit> provinceUnit;
+    public Map<Province,Faction> provinceList;
+    public String address;
+
+    // assign default unit to each province 
+    public Database() throws IOException {
+
+        provinceUnit = new HashMap<Province,Unit>();
+        provinceList = new HashMap<Province,Faction>();
+
+        address = "src/unsw/gloriaromanus/initial_province_ownership.json";
+
+        setProvinceToOwningFactionMap();
+
+        for (Province provinceName : provinceList.keySet()) {
+            Unit unit = new Unit();
+            provinceUnit.put(provinceName, unit);
+          }
+    }
+   
+
+    public void addFaction(String faction) throws IOException {
         addtoFile(faction, "F", " ");
     }
 
@@ -22,9 +45,16 @@ public class Database {
         addtoFile(faction, "P", province);
     }
 
+    /**
+     * adds faction/province to the database
+     * @param faction
+     * @param option
+     * @param province
+     * @throws IOException
+     */
     public void addtoFile(String faction, String option, String province) throws IOException {
 
-        String content = Files.readString(Paths.get("src/unsw/gloriaromanus/initial_province_ownership.json"));
+        String content = Files.readString(Paths.get(address));
         JSONObject ownership = new JSONObject(content);
 
         if (option.equals("F")) {
@@ -38,9 +68,35 @@ public class Database {
             list.put(province);
         }
             // Files.write(path, bytes, options)
-            Files.writeString(Paths.get("src/unsw/gloriaromanus/initial_province_ownership.json"), ownership.toString());
+            Files.writeString(Paths.get(address), ownership.toString());
 
 
         
     }
+
+    /**
+     * Function initilises the map of province to faction 
+     * @return map of province to faction 
+     * @throws IOException
+     */
+
+    private Map<Province, Faction> setProvinceToOwningFactionMap() throws IOException {
+        String content = Files.readString(Paths.get(address));
+        JSONObject ownership = new JSONObject(content);
+        Map<Province, Faction> m = new HashMap<Province, Faction>();
+        for (String key : ownership.keySet()) {
+          // key will be the faction name
+          JSONArray ja = ownership.getJSONArray(key);
+          // value is province name
+          for (int i = 0; i < ja.length(); i++) {
+            String value = ja.getString(i);
+            Province p = new Province(value);
+            Faction f = new Faction(key);
+            m.put(p, f);
+          }
+        }
+        return m;
+      }
+
+    
 }
