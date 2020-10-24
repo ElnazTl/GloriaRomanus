ebcba3a395b25c4284bc46dbb3d6e0d6c373747b
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-10-25 08:29:52 +1100

    added army and units, created class player

--- src/unsw/gloriaromanus/Backend/Province.java
+++ src/unsw/gloriaromanus/Backend/Province.java
@@ -5,4 +5,14 @@ public class Province {
     String name;
     Faction faction;
 
+
+    public Province(String name) {
+        this.name = name;
+    }
+    public void battle(Province enemy) {
+        confirmIfProvincesConnected
+    }
+
+  
+
 }
