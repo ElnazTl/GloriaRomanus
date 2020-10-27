d0259bf641fd13b3395c13c82839c4217749ea53
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-10-27 13:35:58 +1100

    added databsae update

--- src/unsw/gloriaromanus/Backend/Unit.java
+++ src/unsw/gloriaromanus/Backend/Unit.java
@@ -1,5 +1,38 @@
 package unsw.gloriaromanus.Backend;
 
+import java.text.BreakIterator;
+
 public class Unit {
     
+    private String category;
+    private String type;
+    private int movementPoint;
+    /**
+     * Unit u = new Unit(artillery, "")
+     */
+    public Unit(String category, String type) {
+        this.category = category;
+        this.type = type;
+        setmovementPoint();
+    }
+
+    private void setmovementPoint() {
+
+        switch (type) {
+            case "calvary":
+                movementPoint = 15;
+                break;
+
+            case "infantry":
+                movementPoint = 10;
+                break;
+
+            case "artillery":
+                movementPoint = 4;
+                break;
+        }
+
+
+    }
+
 }
