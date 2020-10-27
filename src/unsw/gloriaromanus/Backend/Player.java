34a81825f2fdcad85273ed28d8f16b4a8d0ccffb
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-10-27 13:35:58 +1100

    added databsae update

--- src/unsw/gloriaromanus/Backend/Player.java
+++ src/unsw/gloriaromanus/Backend/Player.java
@@ -1,12 +1,17 @@
 package unsw.gloriaromanus.Backend;
 
+import java.io.IOException;
+
 public class Player {
     private Faction faction;
     public String username;
     Boolean turn;
+    Database database;
 
-    public Player(String username) {
+    public Player(String username, Database database) {
         this.username = username;
+        this.database = database;
+
     }
 
     /**
@@ -37,5 +42,50 @@ public class Player {
     public void endTurn() {
         turn = false;
     }
+
+    /**
+     * given the player province 
+     * @param human
+     * @param enemy
+     * @param d
+     * @return
+     * @throws IOException
+     */
+    public String invade(String human, String enemy ) throws IOException {
+        if (turn) {
+            Province h = new Province(human);
+            Province e = new Province(enemy);
+
+            return h.battle(e, database);
+        }
+        return "It's not your turn";
+    }
+
     
+    /**
+     * if enough gold, buying the required unit for the given province
+     * @param name
+     * @param category
+     * @param province
+     * @return
+     */
+
+    public String getUnit(String name, String category, String province) {
+        if (turn) {
+            Unit u = new Unit (category, name);
+            Province p = new Province(province);
+
+            p.addUnit(database, u);
+
+            // if successful returning message otherwise returning the fail message
+            return "Added unit";
+
+
+        }
+
+        return "It's not your turn";       
+    }
+
+    
+
 }
--- src/unsw/gloriaromanus/Backend/Province.java
+++ src/unsw/gloriaromanus/Backend/Province.java
@@ -1,18 +1,61 @@
 package unsw.gloriaromanus.Backend;
 
+import org.json.*;
+import unsw.gloriaromanus.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
 public class Province {
     
     String name;
     Faction faction;
-
+    ArrayList<Unit> unitList;
 
     public Province(String name) {
         this.name = name;
     }
-    public void battle(Province enemy) {
-        confirmIfProvincesConnected
+
+    // choose a random unit to invade
+    public String battle(Province enemy, Database d) throws IOException {
+        
+        if(!confirmIfProvincesConnected(name, enemy.name)) return ("Provinces not adjacent, cannot invade!");
+        unitList = d.getProvinceUnit().get(this.name);
+
+        Unit humanUnit = chooseUnit(unitList);
+        Unit EnemyUnit = chooseUnit(d.getProvinceUnit().get(enemy.name));
+
+
+        return "true";
+
     }
 
-  
+    private boolean confirmIfProvincesConnected(String province1, String province2) throws IOException {
+        String content = Files
+            .readString(Paths.get("src/unsw/gloriaromanus/province_adjacency_matrix_fully_connected.json"));
+        JSONObject provinceAdjacencyMatrix = new JSONObject(content);
+        return provinceAdjacencyMatrix.getJSONObject(province1).getBoolean(province2);
+    }
+   
+
+    public Unit chooseUnit(List<Unit> u) {
+
+        Random r = new Random();
+        return u.get(r.nextInt(u.size()));
+        
+    }
+
+    /**
+     * given the game database adds the unit to the list of units present in the province
+     * @param d
+     * @param u
+     */
+    public void addUnit(Database d, Unit u) {
+        d.getProvinceUnit().get(this).add(u);
+    }
+    
 
 }
