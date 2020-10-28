7a52be210227374af6772b9939315d4f1af188f5
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-10-28 13:52:23 +1100

    added skirmish to battle resolver and more javadoc for clarity

--- src/unsw/gloriaromanus/Backend/Province.java
+++ src/unsw/gloriaromanus/Backend/Province.java
@@ -8,30 +8,36 @@ import java.util.ArrayList;
 import java.io.IOException;
 
 public class Province {
-    
-    String name;
-    Faction faction;
-    ArrayList<Unit> unitList;
-    Database database;
+
+    public String name;
+    private Faction faction;
+    private ArrayList<Unit> unitList;
+    private Database database;
 
     public Province(String name, Database database) {
         this.name = name;
         this.database = database;
         setFaciton();
         setUnit();
-       
-        
+
     }
 
     private void setUnit() {
         unitList = database.getProvinceUnit().get(this);
     }
+
     private void setFaciton() {
         faction = database.getFactionProvince().get(this);
     }
 
+    public ArrayList<Unit> getUnits() {
+        return unitList;
+    }
+
     /**
-     * checks if tow provinces are adjacent for invasion and chooses random units to battle
+     * checks if tow provinces are adjacent for invasion and chooses random units to
+     * battle
+     * 
      * @param enemy
      * @param d
      * @return
@@ -39,21 +45,21 @@ public class Province {
      */
     public String battle(Province enemy, Database d) throws IOException {
 
-         
-        
-        if(!confirmIfProvincesConnected(name, enemy.name)) return ("Provinces not adjacent, cannot invade!");
+        if (!confirmIfProvincesConnected(name, enemy.name))
+            return ("Provinces not adjacent, cannot invade!");
         unitList = d.getProvinceUnit().get(this.name);
 
-        //TODO: return the result of Battle resolver
-        Battleresolver br = new Battleresolver(this, enemy,database);
-
+        // TODO: return the result of Battle resolver
+        Battleresolver br = new Battleresolver(this, enemy, database);
 
         return "true";
 
     }
 
     /**
-     * Function will check if two provinces are adjacent, implementation taken from controller
+     * Function will check if two provinces are adjacent, implementation taken from
+     * controller
+     * 
      * @param province1
      * @param province2
      * @return
@@ -62,15 +68,15 @@ public class Province {
 
     private boolean confirmIfProvincesConnected(String province1, String province2) throws IOException {
         String content = Files
-            .readString(Paths.get("src/unsw/gloriaromanus/province_adjacency_matrix_fully_connected.json"));
+                .readString(Paths.get("src/unsw/gloriaromanus/province_adjacency_matrix_fully_connected.json"));
         JSONObject provinceAdjacencyMatrix = new JSONObject(content);
         return provinceAdjacencyMatrix.getJSONObject(province1).getBoolean(province2);
     }
-   
-   
 
     /**
-     * given the game database adds the unit to the list of units present in the province
+     * given the game database adds the unit to the list of units present in the
+     * province
+     * 
      * @param d
      * @param u
      */
@@ -85,27 +91,23 @@ public class Province {
 
     /**
      * moves troops between provinces, will fail if can't move the troop
+     * 
      * @param to
      * @param u
      */
-    
 
     public String moveTroopTo(Province to, Unit u) throws IOException {
 
-        
-            // TODO: find the shortes path + movement points to move to for DN
+        // TODO: find the shortes path + movement points to move to for DN
 
+        // units can only move between adjacent provinces for pass mark need to update
+        if (!confirmIfProvincesConnected(to.name, this.name))
+            return "Provinces are not adjacent";
+        database.getProvinceUnit().get(this).remove(u);
+        database.getProvinceUnit().get(to).remove(u);
 
-            // units can only move between adjacent provinces for pass mark need to update
-            if(!confirmIfProvincesConnected(to.name, this.name)) return "Provinces are not adjacent";
-            database.getProvinceUnit().get(this).remove(u);
-            database.getProvinceUnit().get(to).remove(u);
+        return "Successfully moved the unit";
 
-            return "Successfully moved the unit";
-
-
-       
     }
-    
 
 }
