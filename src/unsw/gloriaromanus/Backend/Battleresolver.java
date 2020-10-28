3cbf8cc930498499890cf6aff71665caae75c15a
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-10-28 13:52:23 +1100

    added skirmish to battle resolver and more javadoc for clarity

--- src/unsw/gloriaromanus/Backend/Battleresolver.java
+++ src/unsw/gloriaromanus/Backend/Battleresolver.java
@@ -15,17 +15,40 @@ public class Battleresolver {
         playerProvince = player;
         enemyProvince = enemy;
         this.database = database;
+        runSkirmish(player, enemy);
     }
 
      /**
-     * randomly chooses a unit in the province for battle
-     * @param u
-     */
+      * selets units from each player and battles them against eachother until invasion result is determined
+      * @param player
+      * @param enemy
+      */
+    private void runSkirmish(Province player,Province enemy) {
+        ArrayList<Unit> playerUnitList = player.getUnits();
+        ArrayList<Unit> enemyUnitList = player.getUnits();
+        startSkirmish(chooseUnit(playerUnitList), chooseUnit(enemyUnitList));
+
+    }
+    
+    /**
+    * randomly chooses a unit in the province for battle
+    * @param u
+    */
 
     private Unit chooseUnit(List<Unit> u) {
 
         Random r = new Random();
         return u.get(r.nextInt(u.size()));
         
+    }
+
+    /**
+     * given two units, implements the logic of battle for two units
+     * @param player
+     * @param enemy
+     */
+
+    private void startSkirmish(Unit player, Unit enemy) {
+        //TODO: implement the logic of skirmish 
     }
 }
