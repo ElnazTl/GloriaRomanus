d99452ef724d5ea51aa4b86e3004d700e20a3578
Noah Correa <z5206979@ad.unsw.edu.au>
2020-10-29 19:37:51 -0700

    fix merge conflict issues

--- src/unsw/gloriaromanus/Backend/Province.java
+++ src/unsw/gloriaromanus/Backend/Province.java
@@ -1,29 +1,13 @@
 package unsw.gloriaromanus.Backend;
 
-<<<<<<< HEAD
 import java.util.List;
 import java.util.ArrayList;
 import org.json.JSONObject;
-=======
-
-import org.apache.commons.codec.net.QCodec;
-import org.json.*;
-
-import javafx.scene.control.ListCell;
-import unsw.gloriaromanus.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.io.IOException;
-import java.util.List;
-
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
 
 import unsw.gloriaromanus.Backend.tax.*;
 
 public class Province {
     
-    Database database;
     String faction;
     String name;
     int wealth;
@@ -36,42 +20,11 @@ public class Province {
     public Province() {}
     public Province(String name, Database database) {
         this.name = name;
-<<<<<<< HEAD
         this.units = new ArrayList<Unit>();
         this.unitsTraining = new ArrayList<Unit>(2);
         changeTaxRate(LowTax.TYPE);
     }
-=======
-        this.database = database;
-        setFaciton();
-        units = setUnit();
-        unitsTraining = setTraining();
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
 
-    }
-
-    public void setDatabase(Database d){
-        this.database = d;
-    }
-    
-
-    private List<Unit> setUnit() {
-        
-        return database.getProvinceUnit().get(name);
-    }
-    private List<Unit> setTraining() {
-        return database.getProvinceTraining().get(name);
-    }
-
-    private void setFaciton() {
-        faction = database.getFactionProvince().get(name).name;
-
-    }
-
-<<<<<<< HEAD
-    public TaxRate getTaxRate() {
-        return taxRate;
-    }
 
     /**
      * Called at start of a new turn
@@ -89,12 +42,13 @@ public class Province {
             }
         }
         wealth = wealth + taxRate.getWealth();
-=======
-    public List<Unit> getUnits() {
-        return units;
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
+
     }
 
+    public List<Unit> getUnits() {
+        return units;
+
+    }
     /**
      * checks if tow provinces are adjacent for invasion and chooses random units to
      * battle
@@ -104,7 +58,6 @@ public class Province {
      * @return
      * @throws IOException
      */
-<<<<<<< HEAD
     public Unit findUnit(Long id) {
         for (Unit u : units) {
             if (id == u.getUnitID()) {
@@ -112,16 +65,6 @@ public class Province {
             }
         }
         return null;
-=======
-    public String invade(Province enemy, Database d) throws IOException {
-
-        if (!confirmIfProvincesConnected(name, enemy.name))
-            return ("Provinces not adjacent, cannot invade!");
-
-        // TODO: return the result of Battle resolver
-        Battleresolver br = new Battleresolver(this, enemy, database);
-        return ("true");
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
     }
 
 
@@ -139,7 +82,6 @@ public class Province {
 
 
     /**
-<<<<<<< HEAD
      * Changes the tax rate of the province
      * 
      * @param tax Name of Tax Rate
@@ -148,22 +90,6 @@ public class Province {
         //removeTaxMorale();
         taxRate = TaxFactory.newTaxRate(tax);
         //applyTaxMorale();
-=======
-     * Function will check if two provinces are adjacent, implementation taken from
-     * controller
-     * 
-     * @param province1
-     * @param province2
-     * @return
-     * @throws IOException
-     */
-
-    private boolean confirmIfProvincesConnected(String province1, String province2) throws IOException {
-        String content = Files
-                .readString(Paths.get("src/unsw/gloriaromanus/province_adjacency_matrix_fully_connected.json"));
-        JSONObject provinceAdjacencyMatrix = new JSONObject(content);
-        return provinceAdjacencyMatrix.getJSONObject(province1).getBoolean(province2);
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
     }
 
     /**
@@ -174,7 +100,6 @@ public class Province {
      * @param u
      */
 
-<<<<<<< HEAD
     // TODO 
     // public void applyTaxMorale() {
     //     // Change morale of units
@@ -185,58 +110,13 @@ public class Province {
     // public void removeTaxMorale() {
     //     // Remove morale effect of units/training units
     // }
-=======
-    // private void setWealth(int cost) {
-    //     this.wealth = wealth-cost;
-    // }
 
-    private void setTraining(Unit u) {
-        unitsTraining.add(u);
-    }
-    
-    /**
-     * if enough wealth would get the unit and add it to list of trining units
-     * @param u
-     * @return
-     */
 
-    public String getUnit(Unit u) {
-
-        /**
-         * gold implementation not required for pass
-         * if (wealth >= u.getCost()) {
-
-            database.getProvinceTraining().get(name).add(u);
-            setWealth(u.getCost());
-            setTraining(u);
-            return "Successfully added the unit";
-        }
-        **/
-
-        if (unitsTraining.size() >=2 ) {
-            System.out.println("Already training two troops");
-        }
-        setTraining(u);
-        return "unit started traiing ";
-
-        
-    }
-    /**
-     * will add the unit to unitlist when training is over
-     * @param u
-     */
-    private void addUnit(Unit u) {
-        database.getProvinceUnit().get(name).add(u);
-        units.add(u);
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
-
-    }
 
 
     /**
      * moves troops between provinces, will fail if can't move the troop
      * 
-<<<<<<< HEAD
      * @param name Name of unit to train
      * @return True if training unit, otherwise False
      */
@@ -248,41 +128,12 @@ public class Province {
             // u.applyModifier(taxRate.getMoraleModifier());
 
             return true;
-=======
-     * @param to
-     * @param u
-     */
-
-    public String moveTroopTo(Province to, String u) throws IOException {
-
-        // TODO: find the shortes path + movement points to move to for DN
-
-        // units can only move between adjacent provinces for pass mark need to update
-        if (!to.getFaction().equals(faction)) return "can't move the unit to enemy territoy";
-
-        if (!confirmIfProvincesConnected(to.name, this.name)) return "Provinces are not adjacent";
-        for (int i = 0; i < units.size();i++) {
-            if (units.get(i).getName().equals(u)) {
-
-                to.addUnit(units.get(i));
-                units.remove(i);
-            }
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
         }
-           
-      
-        
-
-        return "Successfully moved the unit";
-
     }
-    /**
-     * function return a list of the name of the units 
-     * @return
-     */
+
+    
 
 
-<<<<<<< HEAD
     /**
      * Resets the province's units and tax rate,
      * does not modify the wealth
@@ -317,15 +168,6 @@ public class Province {
 
 
     // public String moveTroopTo(Province to, Unit u) {
-=======
-    public ArrayList<String> ListOfUnitString() {
-        ArrayList<String> result = new ArrayList<String> ();
-        for (Unit u: getUnits()) {
-            result.add(u.getName());
-        }
-        return result;
-    }
->>>>>>> 4adaecd027a12cc72fc14b0f7a9d5b292a433e28
 
     public String getFaction() {
         return faction;
@@ -340,12 +182,6 @@ public class Province {
 
     public int getWealth() {
         return wealth;
-    }
-
-   
-
-    public TaxFactory getTaxFactory() {
-        return taxFactory;
     }
 
    
@@ -373,10 +209,6 @@ public class Province {
 
     public void setUnits(List<Unit> units) {
         this.units = units;
-    }
-
-    public void setTaxFactory(TaxFactory taxFactory) {
-        this.taxFactory = taxFactory;
     }
 
     public void setTaxRate(TaxRate taxRate) {
