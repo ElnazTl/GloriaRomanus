47b2ec7db731e3d02b78cb808576185504f81481
Noah Correa <z5206979@ad.unsw.edu.au>
2020-11-01 05:18:06 -0800

    added invade test and support

---
+++ src/test/InvadeTest.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import unsw.gloriaromanus.Backend.*;
+
+public class InvadeTest {
+
+    
+    @Test
+    public void TestInvadeAdjacentNoEnemy() throws IOException {
+
+        Database db = new Database("test");
+
+        Player A = db.addNewPlayer("A", "Spain");
+        Player B = db.addNewPlayer("B", "Numidia");
+
+        A.startTurn();
+
+        A.trainUnit("V", "soldier");
+        A.trainUnit("V", "soldier");
+        A.endTurn();
+        B.startTurn();
+        B.endTurn();
+
+        List<Unit> unitsA = A.getUnitsFromProvince("V");
+        System.out.println(unitsA);
+
+        A.selectUnit("V", unitsA.get(0).getUnitID());
+
+        int result = A.invade("V", "VI");
+        assertEquals(1, result);
+
+    }
+
+
+}
+
