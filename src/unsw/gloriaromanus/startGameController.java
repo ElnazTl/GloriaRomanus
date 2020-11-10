eeed81a5cab0c755ec49aad620ba1206d03bae20
Hailey Jung <z5213092@ad.unsw.edu.au>
2020-11-10 03:18:39 -0800

    Added startGameApplication

--- src/unsw/gloriaromanus/startGameController.java
+++ src/unsw/gloriaromanus/startGameController.java
@@ -3,11 +3,26 @@ package unsw.gloriaromanus;
 // import java.io.IOException;
 // import java.net.URL;
 
-// import javafx.event.ActionEvent;
-// import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 // import javafx.scene.control.TextArea;
 // import javafx.scene.control.TextField;
 
 public class startGameController {
+    @FXML
+    private Button backButton;
+
+    private signupScreen signupScreen;
+
+    @FXML
+    public void handleBackButton(ActionEvent event) {
+        signupScreen.start();
+    }
+
+    public void setSignupScreen(signupScreen signupScreen) {
+        this.signupScreen = signupScreen;
+    }
+
     
 }
