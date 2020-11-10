99c9b95896d724f351ba137d986e81b0d87a3f92
Hailey Jung <z5213092@ad.unsw.edu.au>
2020-11-09 23:57:17 -0800

    added codes on controllers and added screen.java files

---
+++ src/unsw/gloriaromanus/startGameController.java
@@ -0,0 +1,13 @@
+package unsw.gloriaromanus;
+
+// import java.io.IOException;
+// import java.net.URL;
+
+// import javafx.event.ActionEvent;
+// import javafx.fxml.FXML;
+// import javafx.scene.control.TextArea;
+// import javafx.scene.control.TextField;
+
+public class startGameController {
+    
+}
