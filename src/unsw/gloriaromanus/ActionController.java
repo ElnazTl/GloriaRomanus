d80c8b77311a72d27c296ed65a3d1d013021361e
Farnaz Tavakol <z5212916@unsw.edu.au>
2020-11-12 18:00:09 +1100

    added action menu to the front end

---
+++ src/unsw/gloriaromanus/ActionController.java
@@ -0,0 +1,24 @@
+package unsw.gloriaromanus;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ActionController extends MenuController {
+    @FXML
+    public void trainUnit() {
+        System.out.println("x");
+    }
+    @FXML
+    public void moveTroop() {
+        
+        System.out.println("x");
+    }
+    @FXML
+    public void invade() {
+        System.out.println("x");
+    }
+
+}
